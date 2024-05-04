a30f2912f7a8b8c25273bb94a7743b03c6492b99
CLINTPS <clintps1010@gmail.com>
2024-05-04 17:06:32 +0530

    feat:Data base chenge second time

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser())
 
 //cors
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://user-managment-mern-redux-toolkit.onrender.com"],
     credentials:true
 }))
 
